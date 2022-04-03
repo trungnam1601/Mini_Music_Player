130f296878eae233568a886864e00cd54c0222b3
trungnam1601 <trungnam16hd@gmail.com>
2022-04-03 22:29:18 +0700

    add project

---
+++ Music_Player/assets/css/main.js
@@ -0,0 +1,69 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const app = {
+    songs: [
+        {
+            name: 'Nevada',
+            singer: 'Vicetone',
+            path: './assets/music/Nevada-Monstercat-6983746.mp3',
+            image: './assets/img/nevada.jpg',
+        },
+        {
+            name: 'SummerTime',
+            singer: 'K-391',
+            path: './assets/music/Summertime-K391-3549537.mp3',
+            image: './assets/img/summerTime.jpg',
+        },
+        {
+            name: 'Monody',
+            singer: 'TheFatRat',
+            path: './assets/music/Monody-TheFatRatLauraBrehm-4174394.mp3',
+            image: './assets/img/monody.jpg',
+        },
+        {
+            name: 'Hoa Hải Đường',
+            singer: 'J-97',
+            path: './assets/music/music/hoa-hai-duong.mp3',
+            image: './assets/img/hoa-hai-duong.jpeg',
+        },
+        {
+            name: 'Khuê Mộc Lang',
+            singer: 'Hương Ly',
+            path: './assets/music/music/khue-moc-lang.mp3',
+            image: './assets/img/khue-moc-lang.jpeg',
+        },
+        {
+            name: 'Rồi Tới Luôn',
+            singer: 'Nal',
+            path: './assets/music/music/roi-toi-luon.mp3',
+            image: './assets/img/roi-toi-luon.jpeg',
+        },
+        {
+            name: 'Sầu Hồng Gai',
+            singer: 'G5R Squad',
+            path: './assets/music/music/sau-hong-gai.mp3',
+            image: './assets/img/sau-hong-gai.jpeg',
+        },
+        {
+            name: 'Thiên Đàng',
+            singer: 'Wowy',
+            path: './assets/music/music/thien-dang.mp3',
+            image: './assets/img/thien-dang.jpeg',
+        },
+        {
+            name: 'Thương nhau tới bến',
+            singer: 'Nal',
+            path: './assets/music/music/thuong-nhau-toi-ben.mp3',
+            image: './assets/img/thuong-nhau-toi-ben.jpeg',
+        },
+        {
+            name: 'Y chang xuân sang',
+            singer: 'Nal',
+            path: './assets/music/music/y-chang-xuan-sang.mp3',
+            image: './assets/img/y-chang-xuan-sang.jpeg',
+        },
+        
+    ],
+}
+
